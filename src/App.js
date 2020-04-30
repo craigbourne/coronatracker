0b42df58acdb83c4647550272146399034469884
Craig Bourne <craigbourne15@gmail.com>
2020-04-30 12:35:49 +0100

    created file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function App() {
-	return <div className="App">Hello!!!!</div>;
+class App extends React.Component {
+	render() {
+		return (
+			<div class="App">
+				<h1>App</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
