631f7d3c0e4d73bd9cc796b0bdd367746769eb9c
Craig Bourne <craigbourne15@gmail.com>
2020-04-30 12:44:22 +0100

    functional components and returning some text for each in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Cards from './Components/Cards/Cards';
+import Chart from './Components/Chart/Chart';
+import CountryPicker from './Components/CountryPicker/CountryPicker';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div class="App">
-				<h1>App</h1>
+				<Cards />
+				<CountryPicker />
+				<Chart />
 			</div>
 		);
 	}
---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cards = () => {
+	return (
+		<div className="cards">
+			<h1>Cards</h1>
+			<h3>This is the cards component</h3>
+		</div>
+	);
+};
+
+export default Cards;
