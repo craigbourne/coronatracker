b885b6ea7c9a085f852856def8f2bb0af473d7b1
Craig Bourne <craigbourne15@gmail.com>
2020-06-05 11:41:09 +0100

    jsx structure for cards

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
 
 	render() {
 		const { data } = this.state;
+
 		return (
 			<div className={styles.container}>
 				<Cards data={data} />
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.module.css';
 
-const Cards = () => {
+const Cards = ({ confirmed, recovered, deaths, lastUpdate }) => {
+	console.log(`cards data:`, props);
 	return (
-		<div className="cards">
-			<h1>Cards</h1>
-			<h3>This is the cards component</h3>
+		<div className="styles.container">
+			<Grid container spacing={3} justify="center">
+				<Grid item component={Card}>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>
+							Infected
+						</Typography>
+						<Typography variant="h5" />
+						<Typography color="textSecondary">DATTEEEE!!!</Typography>
+						<Typography variant="body2">Number of active cases of COVID-19</Typography>
+					</CardContent>
+				</Grid>
+				<Grid item component={Card}>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>
+							Recovered
+						</Typography>
+						<Typography variant="h5">DATAAAA!!!</Typography>
+						<Typography color="textSecondary">DATTEEEE!!!</Typography>
+						<Typography variant="body2">Number of recoveries from COVID-19</Typography>
+					</CardContent>
+				</Grid>
+				<Grid item component={Card}>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>
+							Deaths
+						</Typography>
+						<Typography variant="h5">DATAAAA!!!</Typography>
+						<Typography color="textSecondary">DATTEEEE!!!</Typography>
+						<Typography variant="body2">Number of deaths from COVID-19</Typography>
+					</CardContent>
+				</Grid>
+			</Grid>
 		</div>
 	);
 };
